8c366fb94b98939de50bf489422cf17a66e538f1
joey66666 <2571078340@qq.com>
2019-12-25 12:05:42 +0800

    头插法2

--- leetcode/206.反转链表.java
+++ leetcode/206.反转链表.java
@@ -14,18 +14,35 @@
  * }
  */
 
- // 头插法
+
+ // solution 1, 头插法,head不动
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if (head == null) {
+//             return head;
+//         }
+//         ListNode new_head = head;
+//         while (head.next != null) {
+//             ListNode next = head.next;
+//             head.next = head.next.next;
+//             next.next = new_head;
+//             new_head = next;
+//         }
+//         return new_head;
+
+//     }
+// }
+
+// solution 2, 头插法，head 动
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return head;
-        }
-        ListNode new_head = head;
-        while (head.next != null) {
-            ListNode next = head.next;
-            head.next = head.next.next;
-            next.next = new_head;
-            new_head = next;
+        ListNode new_head = null;
+        while(head!=null)
+        {
+            ListNode next=head.next;
+            head.next=new_head;
+            new_head=head;
+            head=next;
         }
         return new_head;
 
