65ab9ac6841d726b1cd68685bf5183623abd807a
joey66666 <2571078340@qq.com>
2020-02-01 21:31:48 +0800

    53. Maximum Subarray

---
+++ leetcode/53.最大子序和.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+// 暴力
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int max=nums[0];
+//         for(int i=0;i<nums.length;i++){
+//             int temp=0;
+//             for(int j=i;j<nums.length;j++){
+//                 temp+=nums[j];
+//             if(temp>max){
+//                 max=temp;
+//             }
+//         }     
+//         }
+//         return max;
+//     }
+// }
+
+// DP
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
