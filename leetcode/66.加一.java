2571d041fe6712eebde087fe88b89d4a0678d0b3
joey66666 <2571078340@qq.com>
2020-02-10 12:54:00 +0800

    lc 66 plus one

---
+++ leetcode/66.加一.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=java
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] results = new int[digits.length + 1];
+        results[0] = 1;
+        return results;
+
+    }
+}
+// @lc code=end
