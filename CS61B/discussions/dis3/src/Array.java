9f06a0b8407252dd64f5e24cf0955b644c8bb99f
Joey Zhu <joey@Joeys-MBP16.local>
2020-03-22 22:36:58 +0800

    lec7, dis3

--- CS61B/discussions/dis3/src/Array.java
+++ CS61B/discussions/dis3/src/Array.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Array {
+    public static int[] insert(int[] arr, int item, int position) {
+        int[] newArray = new int[arr.length + 1];
+
+        if (position > arr.length) {
+            System.arraycopy(arr, 0, newArray, 0, arr.length);
+            newArray[arr.length] = item;
+            return newArray;
+        }
+
+        int i = 0;
+        while (i < position) {
+            newArray[i] = arr[i];
+            i += 1;
+        }
+
+        newArray[i] = item;
+        System.arraycopy(arr, i, newArray, i + 1, arr.length - i);
+        return newArray;
+    }
+
+    public static void reverse(int[] arr) {
+        int mid = arr.length / 2;
+        int first = 0;
+        int last = arr.length - 1;
+        while (first < mid) {
+            int tmp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = tmp;
+            first++;
+        }
+    }
+
+    /**
+     * [3, 2,1] ->  [3, 3, 3, 2, 2, 1]
+     * non-destructive
+     */
+
+    public static int[] replicate(int[] arr) {
+        int total = 0;
+        for (int item : arr) {
+            total += item;
+        }
+        int[] newArr = new int[total];
+
+        int b = 0;
+        for (int item : arr) {
+            for (int i = 0; i < item; i++) {
+                newArr[b] = item;
+                b++;
+            }
+        }
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int a = 0; a < arr[i]; a++) {
+//                newArr[b] = arr[i];
+//                b++;
+//            }
+//        }
+        return newArr;
+
+    }
+
+    public void testRecerse() {
+        int[] array = {5, 9, 10, 14, 15};
+        System.out.println(Arrays.toString(array));
+        reverse(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void testReplicate() {
+        int[] a = {3, 2, 1};
+        int[] newA = replicate(a);
+        System.out.println(Arrays.toString(newA));
+    }
+
+    public static void main(String[] args) {
+        Array a = new Array();
+        a.testReplicate();
+
+    }
 }
--- CS61B/discussions/dis3/src/SLList.java
+++ CS61B/discussions/dis3/src/SLList.java
@@ -1,4 +1,95 @@
-package PACKAGE_NAME;
+import javax.print.AttributeException;
 
 public class SLList {
+    private class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int item, IntNode next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private IntNode first;
+
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    /*
+     * 在postion处插入x，若postion大于length则插在尾部
+     * */
+    public void insert(int item, int position) {
+        IntNode node = first;
+        for (int i = 0; i < position - 1; i++) {
+            node = node.next;
+            if (node.next == null) {
+                node.next = new IntNode(item, null);
+                break;
+            }
+        }
+
+        IntNode insertNode = new IntNode(item, node.next);
+        node.next = insertNode;
+    }
+
+    public void testInsert() {
+        IntNode node = first;
+
+        insert(10, 1);
+        insert(10, 7);
+
+        while (node.next != null) {
+            System.out.println(node.item);
+            node = node.next;
+        }
+    }
+
+    /* 迭代和递归都实现
+     * */
+    public void reverse() {
+        // 迭代
+        if (first == null | first.next == null) {
+            return;
+        }
+        IntNode node = first.next;
+        first.next = null;
+
+        while (node != null) {
+            IntNode temp = node.next;
+            node.next = first;
+            first = node;
+            node = temp;
+        }
+    }
+
+    public void reverse2() {
+        // 递归
+        if (first == null | first.next == null) {
+            return;
+        }
+        first = reverseHelper(first);
+    }
+
+    public IntNode reverseHelper(IntNode last) {
+        if (last == null | last.next == null) {
+            return last;
+        }
+        IntNode endofReverse = last.next;
+        IntNode reversed = reverseHelper(endofReverse);
+        endofReverse.next = last;
+        last.next = null;
+        return reversed;
+    }
+
+
+    public static void main(String[] args) {
+        SLList list = new SLList();
+        list.addFirst(2);
+        list.addFirst(6);
+        list.addFirst(5);
+        list.reverse2();
+    }
 }
+
