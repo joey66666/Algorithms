f4930c75e71e4f76a144b52a3a1eb42b9a832929
zzjoeyyy@foxmail.com <zzjoeyyy@foxmail.com>
2020-04-08 21:53:58 +0800

    update

--- Leetcode-cn/118.杨辉三角.java
+++ Leetcode-cn/118.杨辉三角.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 /*
  * @lc app=leetcode.cn id=118 lang=java
  *
@@ -9,16 +7,16 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list=new ArrayList<List<Integer>>();
-        int arry[][]=new int[numRows][numRows];
-        List<Integer> list1= null;
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        int arry[][] = new int[numRows][numRows];
+        List<Integer> list1 = null;
 
-        for(int i=0;i<numRows;i++){
-            arry[i][0]=1;
-            list1=new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            arry[i][0] = 1;
+            list1 = new ArrayList<Integer>();
             list1.add(arry[i][0]);
-            for(int j=1;j<=i;j++){
-                arry[i][j]=arry[i-1][j-1]+arry[i-1][j];
+            for (int j = 1; j <= i; j++) {
+                arry[i][j] = arry[i - 1][j - 1] + arry[i - 1][j];
                 list1.add(arry[i][j]);
             }
             list.add(list1);
