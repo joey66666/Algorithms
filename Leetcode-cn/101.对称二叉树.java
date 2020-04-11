dec49114012fde5c3a1b7cf356f080a990b08876
zzjoeyyy@foxmail.com <zzjoeyyy@foxmail.com>
2020-04-11 21:54:34 +0800

    lc 101

---
+++ Leetcode-cn/101.对称二叉树.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (50.73%)
+ * Likes:    716
+ * Dislikes: 0
+ * Total Accepted:    120.6K
+ * Total Submissions: 237.6K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ *
+ *
+ *
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return traverse(root.left, root.right);
+    }
+
+    public boolean traverse(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        else if (left == null || right == null || left.val != right.val) return false;
+        return traverse(left.left, right.right) && traverse(left.right, right.left);
+    }
+}
+// @lc code=end
+
