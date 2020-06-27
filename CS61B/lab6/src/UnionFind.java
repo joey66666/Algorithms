70f951240a18bfada9525376c044191841d42c88
zzjoeyyy@foxmail.com <zzjoeyyy@foxmail.com>
2020-06-27 22:45:09 +0800

    lab6 并查集，完成一半

--- CS61B/lab6/src/UnionFind.java
+++ CS61B/lab6/src/UnionFind.java
@@ -1,51 +1,40 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
-
-    public int[] id;
+    public int[] parentArray;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
-        id = new int[n];
+        parentArray = new int[n];
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
-        try {
-            int i= id[vertex];
-        }catch (Exception e){
-            throw e;
-        }
+        // TODO: 未完全理解意思，待实现
+        int i = parentArray[vertex];
     }
 
     /* Returns the size of the set v1 belongs to. */
+    // root store the -size
+    // e.g: -6 as the size is 6,
     public int sizeOf(int v1) {
-        // TODO
-        int parent = id[v1];
-        int count = 0;
-        for (int i : id) {
-            if (i == parent) count += 1;
+        int count = parentArray[v1];
+        while (count >= 0) {
+            count = parentArray[count];
         }
         return count;
-//        return -1;
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        if (id[v1] != v1) return id[v1];
-        return -1;
+        return parentArray[v1];
+
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        if (id[v1] == id[v2]) return true;
-        return false;
+        return find(v1)==find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -60,7 +49,7 @@ public class UnionFind {
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
+        // TODO: 理解Path-compression方法并实现，为connected()的依赖
         return -1;
     }
 
