b77b86109678e3be8f4eeb25042bec70ef9ccf5a
zzjoeyyy@foxmail.com <zzjoeyyy@foxmail.com>
2020-06-28 22:19:16 +0800

    lab6 完成UnionFind，待测试

--- CS61B/lab6/src/UnionFind.java
+++ CS61B/lab6/src/UnionFind.java
@@ -6,35 +6,38 @@ public class UnionFind {
        vertices are in disjoint sets. */
     public UnionFind(int n) {
         parentArray = new int[n];
+        for (int i = 0; i < n; i += 1) {
+            parentArray[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO: 未完全理解意思，待实现
-        int i = parentArray[vertex];
+        if (vertex < 0 || vertex >= parentArray.length) {
+            throw new IllegalArgumentException("invalid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     // root store the -size
-    // e.g: -6 as the size is 6,
+    // e.g: -6 as the size is 6
     public int sizeOf(int v1) {
-        int count = parentArray[v1];
-        while (count >= 0) {
-            count = parentArray[count];
-        }
-        return count;
+        validate(v1);
+        return -parent(find(v1));
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
+        validate(v1);
         return parentArray[v1];
-
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        return find(v1)==find(v2);
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -43,14 +46,38 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        int size1 = sizeOf(v1);
+        int size2 = sizeOf(v2);
+        if (size1 <= size2) {
+            // v1接到v2上
+            parentArray[find(v2)] -= size1;
+            parentArray[find(v1)] = find(v2);
+        } else {
+            // v2接到v1上
+            parentArray[find(v1)] -= size2;
+            parentArray[find(v2)] = find(v1);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO: 理解Path-compression方法并实现，为connected()的依赖
-        return -1;
+        // Path-Compression: call isConnected(x,y)时连接x,y所有parent到各自的root
+        validate(vertex);
+        int root = vertex;
+        while (parent(root) >= 0) {
+            root = parent(root);
+        }
+
+        //Path-Compression
+        int curParent;
+        while (vertex != root) {
+            curParent = parent(vertex);
+            parentArray[vertex] = root;
+            vertex = curParent;
+        }
+
+        return root;
     }
 
 }
---
+++ CS61B/lab6/src/UnionFindTest.java
@@ -0,0 +1,5 @@
+public class UnionFindTest {
+    // TODO: UnionFind待测试
+    UnionFind union = new UnionFind(10);
+
+}
